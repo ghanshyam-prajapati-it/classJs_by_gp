c12bbf10c46ba56bdda0d6b5b8bad3829c797c15
Ghanshyam Prajapati <89632088+ghanshyam-prajapati-it@users.noreply.github.com>
2021-12-21 11:36:39 +0530

    Add files via upload

---
+++ class.js
@@ -0,0 +1,29 @@
+
+    class mainClass {
+        nam = "Ghanshyam";    
+
+        func()
+        {
+            console.log("Krishna");
+        }
+    }
+
+    var store = new mainClass;
+    console.log(store.nam);    
+    console.log(store.func());
+
+    /* class include
+    {
+        x = "Ghanshyam Prajapati";
+
+        funcA ()
+        {
+            console.log("Hello");
+        }
+    }
+
+    var a = new(include);
+    alert(a.x);    
+    console.log(a.funcA()); */
+    
+
